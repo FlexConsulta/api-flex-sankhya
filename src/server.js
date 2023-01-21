7df0094b08b7fabd8da69eba7b73b83defe096ef
Anael Medeiros <anael.medeiros@CTNAPPLE71P6QCM.local>
2023-01-21 14:42:36 -0400

    add rotina para sincronizar 1x ao dia

--- src/server.js
+++ src/server.js
@@ -19,16 +19,39 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 const connectSankhya = async () => {
+  await SankhyaServiceDriver(syncTypes.created);
+  console.log("Sync dirvers created");
+  await SankhyaServiceOwner(syncTypes.created);
+  console.log("Sync owners created");
+  await SankhyaServiceVehicle(syncTypes.created);
+  console.log("Sync veichiles created");
   await SankhyaServiceTravel(syncTypes.created);
+  console.log("Sync travels created");
+
+  await SankhyaServiceDriver(syncTypes.updated);
+  console.log("Sync dirvers updated");
+  await SankhyaServiceOwner(syncTypes.updated);
+  console.log("Sync owners updated");
+  await SankhyaServiceVehicle(syncTypes.updated);
+  console.log("Sync veichicles updated");
   await SankhyaServiceTravel(syncTypes.updated);
-  console.log("Process finished")
+  console.log("Sync traves updated");
+
+  console.log("Process finished");
 };
 
-const teste = async () => {
-  await connectSankhya();
+const checkTime = () => {
+  setTimeout(async () => {
+    const [hora, minuto] = new Date().toLocaleTimeString().split(":");
+
+    if (hora == 15 && minuto == 59) {
+      await connectSankhya();
+    }
+    checkTime();
+  }, 6000); //60000
 };
 
-teste();
+checkTime();
 
 app.listen(process.env.PORT, async () => {
   console.log(`App started on ${process.env.PORT} 👍 `);
