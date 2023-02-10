3b5412cd498c110a581831c518e92cec57267e33
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-02-10 17:03:21 -0400

    começando rotina de motoristas no dbExplorer

--- src/server.js
+++ src/server.js
@@ -1,62 +1,62 @@
-import express from "express";
-import "express-async-errors";
-import morgan from "morgan";
-import cors from "cors";
+import express from 'express';
+import 'express-async-errors';
+import morgan from 'morgan';
+import cors from 'cors';
 
-import "dotenv/config";
-import { SankhyaServiceVehicle } from "./services/sankhya/sankhya.vehicle.js";
-import { syncTypes } from "./shared/syncTypes.js";
-import { SankhyaServiceOwner } from "./services/sankhya/sankhya.owner.js";
-import { SankhyaServiceDriver } from "./services/sankhya/sankhya.driver.js";
-import { SankhyaServiceTravel } from "./services/sankhya/sankhya.travel.js";
+import 'dotenv/config';
+import { SankhyaServiceVehicle } from './services/sankhya/sankhya.vehicle.js';
+import { syncTypes } from './shared/syncTypes.js';
+import { SankhyaServiceOwner } from './services/sankhya/sankhya.owner.js';
+import { SankhyaServiceDriver } from './services/sankhya/sankhya.driver.js';
+import { SankhyaServiceTravel } from './services/sankhya/sankhya.travel.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api", express.static("public"));
+app.use('/api', express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 const connectSankhya = async (syncOptions) => {
-  console.log("Process started");
+  console.log('Process started');
   if (syncOptions.driver) {
-    console.log(" ");
-    console.log("Sync dirvers started");
+    console.log(' ');
+    console.log('Sync dirvers started');
     await SankhyaServiceDriver(syncTypes.created);
-    console.log("Sync dirvers created");
+    console.log('Sync dirvers created');
     await SankhyaServiceDriver(syncTypes.updated);
-    console.log("Sync dirvers updated");
+    console.log('Sync dirvers updated');
   }
 
   if (syncOptions.owner) {
-    console.log(" ");
-    console.log("Sync owners started");
+    console.log(' ');
+    console.log('Sync owners started');
     await SankhyaServiceOwner(syncTypes.created);
-    console.log("Sync owners created");
+    console.log('Sync owners created');
     await SankhyaServiceOwner(syncTypes.updated);
-    console.log("Sync owners updated");
+    console.log('Sync owners updated');
   }
 
   if (syncOptions.veichile) {
-    console.log(" ");
-    console.log("Sync veichiles started");
+    console.log(' ');
+    console.log('Sync veichiles started');
     await SankhyaServiceVehicle(syncTypes.created);
-    console.log("Sync veichiles created");
+    console.log('Sync veichiles created');
     await SankhyaServiceVehicle(syncTypes.updated);
-    console.log("Sync veichicles updated");
+    console.log('Sync veichicles updated');
   }
 
   if (syncOptions.travel) {
-    console.log(" ");
-    console.log("Sync travels started");
+    console.log(' ');
+    console.log('Sync travels started');
     await SankhyaServiceTravel(syncTypes.created);
-    console.log("Sync travels created");
+    console.log('Sync travels created');
     await SankhyaServiceTravel(syncTypes.updated);
-    console.log("Sync traves updated");
+    console.log('Sync traves updated');
   }
-  console.log(" ");
-  console.log("Process finished");
+  console.log(' ');
+  console.log('Process finished');
 };
 
 // const checkTime = (time, sleep) => {
@@ -69,6 +69,11 @@ const connectSankhya = async (syncOptions) => {
 
 app.listen(process.env.PORT, async () => {
   console.log(`App started on ${process.env.PORT} 👍 `);
-  connectSankhya({ driver: true, owner: true, veichile: true, travel: true });
+  connectSankhya({
+    driver: true,
+    owner: false,
+    veichile: false,
+    travel: false,
+  });
   // checkTime(1, 1);
 });
--- src/services/sankhya/sankhya.driver.js
+++ src/services/sankhya/sankhya.driver.js
@@ -1,14 +1,15 @@
-import { apiMge } from "./api.js";
-import { SankhyaServiceAuthenticate } from "./sankhya.authenticate.js";
-import "dotenv/config";
-import { syncTypes } from "../../shared/syncTypes.js";
-import { LogsIntegration } from "../../models/logs_integration.js";
-import { prisma } from "../../database/prismaClient.js";
-import { getDateTimeFromString } from "../utils/dateTime.js";
-import { stateTypes } from "../../shared/stateTypes.js";
-import { tableTypes } from "../../shared/tableTypes.js";
-import { ModelDriver } from "../../models/drivers.js";
-import { enum_status_motorista } from "@prisma/client";
+import { apiMge } from './api.js';
+import { SankhyaServiceAuthenticate } from './sankhya.authenticate.js';
+import 'dotenv/config';
+import { syncTypes } from '../../shared/syncTypes.js';
+import { LogsIntegration } from '../../models/logs_integration.js';
+import { prisma } from '../../database/prismaClient.js';
+import { getDateFormated, getDateTimeFromString } from '../utils/dateTime.js';
+import { stateTypes } from '../../shared/stateTypes.js';
+import { tableTypes } from '../../shared/tableTypes.js';
+import { ModelDriver } from '../../models/drivers.js';
+import { enum_status_motorista } from '@prisma/client';
+import { findFieldIndex } from '../utils/findFieldIndex.js';
 
 const refreshStatusDriver = async (dataParsed) => {
   let modelDriver = new ModelDriver();
@@ -23,10 +24,10 @@ const refreshStatusDriver = async (dataParsed) => {
     if (!id)
       throw new Error(`Motorista não encontrado ${newStatusDriver.cpf_mot}`);
 
-    newStatusDriver["idmotorista"] = id;
-    newStatusDriver["idcliente"] = Number(process.env.ID_CUSTOMER);
-    newStatusDriver["dt_cliente"] = newStatusDriver.dt_criacao;
-    newStatusDriver["status_motorista"] =
+    newStatusDriver['idmotorista'] = id;
+    newStatusDriver['idcliente'] = Number(process.env.ID_CUSTOMER);
+    newStatusDriver['dt_cliente'] = newStatusDriver.dt_criacao;
+    newStatusDriver['status_motorista'] =
       newStatusDriver.ativo === true
         ? enum_status_motorista.Ativo
         : enum_status_motorista.Vencido;
@@ -116,11 +117,11 @@ const createNewDriver = async (dataParsed) => {
 
   if (newDrivers.length > 0) {
     //console.log("newDrivers", newDrivers);
-    await prisma.motorista.createMany({
+    const data = await prisma.motorista.createMany({
       data: newDrivers,
       skipDuplicates: true,
     });
-    //console.log("motoristas criados", data);
+    console.log('motoristas criados', data);
   }
 
   //console.log("motoristas sankhya", dataParsed.length);
@@ -160,34 +161,15 @@ export async function SankhyaServiceDriver(syncType) {
     );
 
   const requestBody = (page) => {
-    const criteria = lastSync
-      ? {
-          expression: {
-            $:
-              syncType == syncTypes.created
-                ? `this.MOTORISTA = 'S' AND this.TIPPESSOA = 'F' AND this.AD_DTINCLUSAO > TO_DATE('${lastSync}', 'dd/mm/yyyy HH24:MI:SS')`
-                : `this.MOTORISTA = 'S' AND this.TIPPESSOA = 'F' AND this.DTALTER > TO_DATE('${lastSync}', 'dd/mm/yyyy HH24:MI:SS')`,
-          },
-        }
-      : {
-          expression: {
-            $: `this.MOTORISTA = 'S' AND this.TIPPESSOA = 'F' `,
-          },
-        };
+    const where = lastSync
+      ? syncType == syncTypes.created
+        ? `AND DTCAD >= TO_DATE('${lastSync}', 'dd/mm/yyyy HH24:MI:SS')`
+        : `AND DATAFLEX >= TO_DATE('${lastSync}', 'dd/mm/yyyy HH24:MI:SS')`
+      : ` `;
 
     return {
       requestBody: {
-        dataSet: {
-          rootEntity: "Parceiro",
-          includePresentationFields: "S",
-          offsetPage: page,
-          criteria,
-          entity: {
-            fieldset: {
-              list: "NOMEPARC,CGC_CPF,AD_N_REG_CNH,AD_EMIS_AT_CNH,AD_DT_PRIM_HAB,DTNASC,ATIVO,AD_DTINCLUSAO,DTALTER,CODPARC,BLOQUEADO",
-            },
-          },
-        },
+        sql: `SELECT NOMEPARC, CGC_CPF, CNH, STATUS, DTCAD, DATAFLEX FROM AD_VWTBCFLEXMOT WHERE CGC_CPF IS NOT NULL ${where}`,
       },
     };
   };
@@ -196,37 +178,39 @@ export async function SankhyaServiceDriver(syncType) {
 
   const getData = async (page) => {
     try {
-      console.log(page, syncType, "page");
+      console.log(page, syncType, 'page');
 
       let dataRequestBody = requestBody(page);
 
       let response = await apiMge.get(
-        `service.sbr?serviceName=CRUDServiceProvider.loadRecords&outputType=json`,
+        `service.sbr?serviceName=DbExplorerSP.executeQuery&outputType=json`,
         { data: { ...dataRequestBody } }
       );
 
-      const totalRecords = response.data.responseBody.entities.total;
-      let data = Array.isArray(response.data.responseBody.entities.entity)
-        ? response.data.responseBody.entities.entity
-        : [response.data.responseBody.entities.entity];
+      //const totalRecords = response.data.responseBody.entities.total;
+
+      let fields = Array.isArray(response.data.responseBody.fieldsMetadata)
+        ? response.data.responseBody.fieldsMetadata
+        : [response.data.responseBody.fieldsMetadata];
+
+      let data = Array.isArray(response.data.responseBody.rows)
+        ? response.data.responseBody.rows
+        : [response.data.responseBody.rows];
 
       if (!data) return;
 
-      let dataParsed = data
-        .filter((item) => item?.f1?.$)
-        .map((item) => {
-          return {
-            nome_mot: item.f0.$,
-            cpf_mot: item.f1.$,
-            cnh_mot: item.f2?.$,
-            dt_emissao_cnh: getDateTimeFromString(item.f3?.$, true),
-            dt_primeira_cnh: getDateTimeFromString(item.f4?.$, true),
-            dt_nascimento: getDateTimeFromString(item.f5?.$, true),
-            ativo: item?.f10?.$ != "S" && item.f6.$ == "S",
-            dt_criacao: getDateTimeFromString(item?.f7?.$),
-            dt_atualizacao: getDateTimeFromString(item?.f8?.$),
-          };
-        });
+      let dataParsed = data.map((item) => {
+        return {
+          nome_mot: item[findFieldIndex('NOMEPARC', fields)],
+          cpf_mot: item[findFieldIndex('CGC_CPF', fields)],
+          cnh_mot: item[findFieldIndex('CNH', fields)],
+          ativo: item[findFieldIndex('STATUS', fields)],
+          dt_criacao: getDateFormated(item[findFieldIndex('DTCAD', fields)]),
+          dt_atualizacao: getDateFormated(
+            item[findFieldIndex('DATAFLEX', fields)]
+          ),
+        };
+      });
 
       if (syncType == syncTypes.created) {
         await createNewDriver(dataParsed);
@@ -247,21 +231,21 @@ export async function SankhyaServiceDriver(syncType) {
       );
 
       await logsIntegration.updateSync(logId, page, stateTypes.inProgress); // gravar dados de sincronizacao no banco de dados (data e hora e tipo, se foi created ou updated), pagina, nome do sincronismo
-      if (process.env.SANKHYA_PAGINATION == totalRecords) {
-        await getData(page + 1);
-      } else {
-        console.log(syncType, stateTypes.success);
-        //fazer updateStatus success
-        await logsIntegration.updateSync(logId, page, stateTypes.success);
-      }
+      // if (process.env.SANKHYA_PAGINATION == totalRecords) {
+      //   await getData(page + 1);
+      // } else {
+      //   console.log(syncType, stateTypes.success);
+      //   //fazer updateStatus success
+      await logsIntegration.updateSync(logId, page, stateTypes.success);
+      // }
     } catch (error) {
       console.log(`Error on getData with page ${page}:`, error);
       //faz updateStatus error
       await logsIntegration.updateSync(logId, page, stateTypes.error);
 
-      if (process.env.IGNORE_ERROR == "YES") {
-        await getData(page + 1);
-      }
+      // if (process.env.IGNORE_ERROR == 'YES') {
+      //   await getData(page + 1);
+      // }
     }
   };
 
