2edc85b21ead99b84bb8b4be212365a3942b7550
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-02-14 17:13:01 -0400

    add get cpf_mot, cpf_prop e placa

--- src/services/sankhya/travels/index.js
+++ src/services/sankhya/travels/index.js
@@ -1,15 +1,15 @@
-import "dotenv/config";
-import { syncTypes } from "../../../shared/syncTypes.js";
-import { getDateFormated } from "../../utils/dateTime.js";
-import { stateTypes } from "../../../shared/stateTypes.js";
-import { tableTypes } from "../../../shared/tableTypes.js";
-import { getLastSync, updateLog } from "../logs.controller.js";
-import { getSankhyaData } from "../api.data.js";
-import { findFieldIndex } from "../../utils/findFieldIndex.js";
-import { createNewTravels } from "./create.js";
-import { updateTravels } from "./update.js";
-import { showLog } from "../../utils/memory.js";
-import { enum_viagem_cancelado } from "@prisma/client";
+import 'dotenv/config';
+import { syncTypes } from '../../../shared/syncTypes.js';
+import { getDateFormated } from '../../utils/dateTime.js';
+import { stateTypes } from '../../../shared/stateTypes.js';
+import { tableTypes } from '../../../shared/tableTypes.js';
+import { getLastSync, updateLog } from '../logs.controller.js';
+import { getSankhyaData } from '../api.data.js';
+import { findFieldIndex } from '../../utils/findFieldIndex.js';
+import { createNewTravels } from './create.js';
+import { updateTravels } from './update.js';
+import { showLog } from '../../utils/memory.js';
+import { enum_viagem_cancelado } from '@prisma/client';
 
 export async function SankhyaServiceTravel(syncType) {
   const syncTable = tableTypes.viagens;
@@ -31,26 +31,29 @@ export async function SankhyaServiceTravel(syncType) {
       let dataParsed = data.map((item) => {
         return {
           idcliente: Number(process.env.ID_CUSTOMER),
-          numero_cliente: item[findFieldIndex("NUVIAGEM", fields)].toString(),
-          dt_viagem: getDateFormated(item[findFieldIndex("DTEMISSAO", fields)]),
-          mercadoria: item[findFieldIndex("PRODUTO", fields)],
-          cidade_origem: item[findFieldIndex("CIDADEUFORIGEM", fields)],
-          cidade_destino: item[findFieldIndex("CIDADEUFDESTINO", fields)],
-          carreta1: item[findFieldIndex("CAR1PLACA", fields)],
-          carreta2: item[findFieldIndex("CAR2PLACA", fields)],
-          carreta3: item[findFieldIndex("CAR3PLACA", fields)],
+          numero_cliente: item[findFieldIndex('NUVIAGEM', fields)].toString(),
+          dt_viagem: getDateFormated(item[findFieldIndex('DTEMISSAO', fields)]),
+          mercadoria: item[findFieldIndex('PRODUTO', fields)],
+          cidade_origem: item[findFieldIndex('CIDADEUFORIGEM', fields)],
+          cidade_destino: item[findFieldIndex('CIDADEUFDESTINO', fields)],
+          carreta1: item[findFieldIndex('CAR1PLACA', fields)],
+          carreta2: item[findFieldIndex('CAR2PLACA', fields)],
+          carreta3: item[findFieldIndex('CAR3PLACA', fields)],
+          cpf_motorista: item[findFieldIndex('MOTCPF', fields)],
+          cpf_cnpj_proprietario: item[findFieldIndex('PROPCPFCNPJ', fields)],
+          placa_veiculo: item[findFieldIndex('CAVPLACA', fields)],
           viagem_cancelado:
-            item[findFieldIndex("STATUS", fields)] > 0
+            item[findFieldIndex('STATUS', fields)] > 0
               ? enum_viagem_cancelado.S
               : enum_viagem_cancelado.N,
           dt_cliente: getDateFormated(
-            item[findFieldIndex("DTEMISSAO", fields)]
+            item[findFieldIndex('DTEMISSAO', fields)]
           ),
           dt_criacao: getDateFormated(
-            item[findFieldIndex("DTEMISSAO", fields)]
+            item[findFieldIndex('DTEMISSAO', fields)]
           ),
           dt_atualizacao: getDateFormated(
-            item[findFieldIndex("DATAFLEX", fields)]
+            item[findFieldIndex('DATAFLEX', fields)]
           ),
         };
       });
@@ -58,9 +61,9 @@ export async function SankhyaServiceTravel(syncType) {
       // console.log("----->", dataParsed);
 
       if (syncType == syncTypes.created) {
-        await createNewTravels(dataParsed);
+        await createNewTravels([...dataParsed]);
       } else {
-        await updateTravels(dataParsed);
+        await updateTravels([...dataParsed]);
       }
 
       dataParsed = null;
